3f1a6798acd4ebd657e0d010df632916c8d052cf
Joel Rodrigues <89555351+Yonzzx@users.noreply.github.com>
2022-06-12 21:58:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const ListDirectory = require('./ListDirectory.js')
-const TOKEN = '5224056154:AAHruF4BuNctnOl6JLBSfYNwGtDx6DSAS6o';
-const url = 'https://e739-87-249-134-130.ngrok.io';
-const port = 4000;
+const {TOKEN,URL,PORT} = process.env
 
-
-
-// No need to pass any parameters as we will handle the updates with Express
 const bot = new TelegramBot(TOKEN, {webHook:true});
 
-// This informs the Telegram servers of the new webhook.
  bot.setWebHook(`${url}/bot${TOKEN}`); 
 bot.getMe((req,res)=>{
 console.log(req)
@@ -41,28 +35,15 @@ bot.on('message', async msg => {
     case '/photos':
       let photos = await ListDirectory('./photos'); 
       bot.sendMessage(msg.chat.id, 'Carregando...')
-      bot.sendChatAction('-1001567360573', 'upload_photo')
-      bot.sendMediaGroup('-1001567360573', photos) 
-      console.log(msg)
       break;
 
     case '/videos':
       let videos = await ListDirectory('./video')
       bot.sendMessage(msg.chat.id, 'Carregando...')
-      bot.sendMediaGroup(msg.chat.id, videos)
-      console.log(msg)
        break;
-
-       case '/canal':
-        let CanalVideo = await ListDirectory('./video');
-        bot.sendMessage(msg.chat.id, 'Carregando...')
-        bot.sendMediaGroup('-1001567360573', CanalVideo)
-        console.log(msg)
-        break;
   
     default:
       bot.sendMessage(msg.chat.id, 'ola')
-      console.log(msg)
       break;
   }
 })
